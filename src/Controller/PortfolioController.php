7f9a46bb3ca98c35778f54ecac0bc2c3445fd745
Neau Matisse <neaumatisse@gmail.com>
2026-01-08 15:16:04 +0100

    OUIIIIIIIIIIIII

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PortfolioController extends AbstractController
 {
+    #[Route('/portfolio', name: 'portfolio')]
     public function index(): Response
     {
         return $this->render('portfolio/index.html.twig');
